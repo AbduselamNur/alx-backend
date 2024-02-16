94cbac66f6290927e261fd30cf6d21b6817daf83
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-02-15 20:05:50 -0800

    Add LIFO Cache implementation

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""LIFO Caching"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO Cache class"""
+
+    def __init__(self):
+        """Initialize"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.order.remove(key)
+            else:
+                if len(self.cache_data) >= self.MAX_ITEMS:
+                    discard = self.order.pop()
+                    del self.cache_data[discard]
+                    print("DISCARD: {}".format(discard))
+                    self.order.append(key)
+                else:
+                    self.order.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key in self.cache_data or key is not None:
+            return self.cache_data[key]
+        return None
