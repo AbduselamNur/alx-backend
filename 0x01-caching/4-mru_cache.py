c4cfd0e2d97b81409b9fa6d3a6027b4343321efe
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-02-15 21:12:03 -0800

    Add MRU Cache implementation

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" MRU Caching
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRU Caching """
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.cache = []
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            value = key not in self.cache_data
+            if len(self.cache_data) >= self.MAX_ITEMS and value:
+                discard = self.cache.pop()
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
+            self.cache_data[key] = item
+            if key in self.cache:
+                self.cache.remove(key)
+            self.cache.append(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            self.cache.remove(key)
+            self.cache.append(key)
+            return self.cache_data[key]
+        return None
