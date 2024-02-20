0d12ed18f87b283e5d8afd279eb36f5a57f169ef
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-02-19 19:19:41 -0800

    Update templates and app.py for internationalization

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Force locale with URL parameter"""
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
@@ -15,6 +15,31 @@ app = Flask(__name__)
 babel = Babel(app)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> dict:
+    """ GET user
+    Return: user or None
+    """
+    try:
+        return users.get(int(request.args.get('login_as')))
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request() -> None:
+    """ GET user
+    Return: user or None
+    """
+    user = get_user()
+    g.user = user
 
 
 @babel.localeselector
@@ -31,9 +56,9 @@ def get_locale() -> str:
 @app.route('/')
 def index():
     """ GET /
-    Return: 4-index.html
+    Return: 5-index.html
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
